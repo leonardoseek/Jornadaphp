68f1c5f167a38dab30a2baaf7d92ff2864af95ed
Leonardo <90659018+leonardoseek@users.noreply.github.com>
2022-12-01 12:40:21 -0300

    Primeiro commit do projeto
    Site básico feito com html e css com o primeiro olá mundo em php

---
+++ JORNADA-PHP/basico/ola.php
@@ -0,0 +1,14 @@
+<div class="titulo"> Olá PHP </div>
+
+<h2>Exemplo A</h2>
+<?php 
+ echo'Olá <br>';
+ echo'Mundo';
+
+?>
+
+<h2>Exemplo B</h2>
+<?= "outra forma de expressar !" ?>
+
+<?php
+phpinfo();
