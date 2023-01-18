7bdbf5a5f6502874961c61b73775707f3fcc3b4e
Leonardo <90659018+leonardoseek@users.noreply.github.com>
2023-01-17 22:25:14 -0300

    add string
    adicionando modulo string

---
+++ JORNADA-PHP/tipos/string.php
@@ -0,0 +1,22 @@
+<div class="titulo">Tipo String </div>
+
+<?php
+echo 'Eu sou uma String';
+echo'<br>';
+var_dump("eu também");
+echo'<br>';
+
+// concatenação
+echo "Nós Também" ." somos";
+echo "<br>", 'Nós também', ' aceitamos virgulas';
+
+print("<br>Também existe a função print");
+
+// algumas funçoes
+
+echo'<br>' . strtoupper('maximizado');
+echo'<br>' . strtolower('MINIMIZADO');
+echo'<br>' . ucfirst('só a primeira letra');
+echo'<br>' . ucwords('todas as palavras');
+echo'<br>' . strlen('Quantas letras?');
+?>
